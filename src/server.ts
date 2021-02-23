546500359c5a6d5c1b7e4bb61106737369f3dbfd
Alexandre <dev.ads.github@gmail.com>
2021-02-23 10:19:07 -0300

    2 dia

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,27 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running!"));
+/**
+* Get => Busrcar
+* Post => Salvar
+* Put => Alterar
+* Delete => Deletar
+* Patch => Alteração específica
+**/
+
+// http://localhost:3333/users
+app.get("/", (request, response) => {
+    
+return response.json({ massage: "hello world - NLW4"});
+})
+
+//1 parametro = rota(recurso API)
+//2 parametro = request, response
+
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar 
+    return response.json({ massage: "Os dados foram salvos com sucesso!"});
+});
+
+
+app.listen(3333, () => console.log("Funfou"));
